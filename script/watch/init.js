6f4b0c3d263f98d69d395cb5f969017176b4f77c
novlan1 <1576271227@qq.com>
2025-05-31 21:25:36 +0800

    docs: add comment

--- script/watch/init.js
+++ script/watch/init.js
@@ -8,6 +8,8 @@ function main() {
   'utils',
   ]);
   execCommand('rm -rf src/packages/press-hor-swiper-light', process.cwd(), 'inherit');
+
+  // 需要删除 press-ui 原项目的 postcss.config.js，否则编译器会找 uni-app 相关的依赖，报错
   execCommand('rm -rf src/press-ui/postcss.config.js', process.cwd(), 'inherit')
 }
 
